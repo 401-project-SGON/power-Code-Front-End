16ddeddd4fe240d17e4a595e0f675e54bd58ae4e
obada <obadaquraan@yahoo.com>
2020-03-20 22:06:33 +0200

    auth

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
-import react from 'react';
+import React from 'react';
 
+import Auth from './components/auth/auth.js';
+import Login from './components/auth/login.js';
+import LoginContext from './components/auth/context.js';
 
-const App =(props)=>{
+const EditLink = props => {
+  return (
+    <Auth capability="update">
+      <span>Edit</span>
+    </Auth>
+  );
+};
 
+const DeleteLink = props => {
+  return (
+    <Auth capability="delete">
+      <span>Delete</span>
+    </Auth>
+  );
+};
 
+const App =(props)=> {
 
-    
+   
+    return (
+      <LoginContext>
+        <Login />
+        <hr />
+        <EditLink />
+        <DeleteLink />
+      </LoginContext>
+    );
+  
 }
 
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import react from 'react'
-import ReactDOM from 'react-dom'
-import App from './app.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
 
-class Main extends React.Component {
-    render() {
-      return <App />;
-    }
-  }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<Main />, rootElement);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
