e7cc909e9c3c96264426764b760d3e9a556ea79b
obada <obadaquraan@yahoo.com>
2020-03-25 16:54:51 +0200

    fixing

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -3,17 +3,18 @@ import { connect } from 'react-redux'
 import { getData ,choose,levelsRendered} from '../../store/actions.js'
 import {Switch,Route,Link,NavLink} from "react-router-dom";
 import {If,Then} from '../if'
+import './data.css'
 
 
 
 const Data = (props) => {
 
     useEffect(()=>{
-        props.getData()
+        if(!props.reducer.renderd){props.getData()}
     },[])
 
     return (
-        <section>
+        <section className='coursesN'>
 
             {/* <button onClick={() => !props.reducer.renderd&&props.getData()}>get courses</button> */}
 
@@ -22,20 +23,22 @@ const Data = (props) => {
                     console.log('item : ', item);
                     return (
                         <li onClick={()=>props.choose(item.courseName)} key={item._id}>
-                            {item.courseName}
+                            {/* {item.courseName} */}
+                            <NavLink to='/course'> {item.courseName}</NavLink>
+
                         </li>
                     )
                 })}
             </ul>
 
-            <If condition={props.reducer.chosen&&!props.reducer.levelsRendered}>
+            {/* <If condition={props.reducer.chosen&&!props.reducer.levelsRendered}>
                 <Then>
                    
                     <NavLink to='/course'> Show</NavLink>
                 
 
                 </Then>
-            </If>
+            </If> */}
             
 
         </section>
