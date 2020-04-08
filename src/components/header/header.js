5b675e9f1ab50664cae1feeb4074328eb0b2513c
obadeh <obadaquraan@yahoo.com>
2020-04-09 02:56:09 +0300

    nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,8 +6,8 @@ import { If, Then } from './../if'
 import Signup from './../auth/signup.js'
 import Login from './../auth/login.js';
 import Chat from '../chat/chat.js'
-
 import './header.css'
+
 
 
 
@@ -18,7 +18,7 @@ const ChatBox = () => {
                 <Dropdown.Toggle as={'href'} id="dropdown-basic-button" title="let's chat">
 
                     Let's Chat
-</Dropdown.Toggle>
+                </Dropdown.Toggle>
                 <Dropdown.Menu >
                     <Dropdown.Item ><Chat /></Dropdown.Item>
                 </Dropdown.Menu>
@@ -29,62 +29,115 @@ const ChatBox = () => {
 }
 
 
-
-const Header = (props) => {
-
+const nav = (props) => {
     return (
-        // <div className='mainHeader'>
-        <Navbar bg="light" variant="dark" sticky="top">
-            <Navbar.Brand><NavLink to='/'><img src='https://i.ibb.co/k3Rq69C/Screenshot-477.png' style={{ borderRadius: '12px' }} height='45' width='50' /></NavLink></Navbar.Brand>
-            <If condition={props.reducer.loggedIn}><Then>
-
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+            <Navbar.Brand><NavLink to='/'><img src='https://i.ibb.co/k3Rq69C/Screenshot-477.png' style={{ borderRadius: '10px' }} height='45' width='50' /></NavLink></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/data'>Courses</NavLink></Nav.Link>
-                    <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/code'>Code Editor</NavLink></Nav.Link>
-                    {/* <Nav.Link ><NavLink to='/feedback'><img src='https://cdn1.iconfinder.com/data/icons/feedback-review/64/x-05-512.png' height='30' width='35' /></NavLink></Nav.Link> */}
-                    <ChatBox />
+                    <If condition={props.reducer.loggedIn}><Then>
 
-                </Nav>
-            </Then></If>
-            <Form inline>
-
-                <Signup className='signup' />
-
-                <If condition={!props.reducer.loggedIn} ><Then>
-                    <Login />
-
-                </Then></If>
+                        <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/data'>Courses</NavLink></Nav.Link>
+                        <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/code'>Code Editor</NavLink></Nav.Link>
+                        {/* <Nav.Link ><NavLink to='/feedback'><img src='https://cdn1.iconfinder.com/data/icons/feedback-review/64/x-05-512.png' height='30' width='35' /></NavLink></Nav.Link> */}
+                        <ChatBox />
 
 
-
-                <If condition={props.reducer.admin} ><Then>
-
-                    <Nav.Link ><NavLink to='/admintool'><img src='https://cdn2.hubspot.net/hub/189007/file-526634248-png/google-apps-admin-panel-icon.png' height='35' width='35' /></NavLink></Nav.Link>
                     </Then></If>
 
-                <If condition={props.reducer.loggedIn}>
-                    <Then>
-                        <NavDropdown style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} title={props.reducer.user.username} id="basic-nav-dropdown">
-                            <NavDropdown.Item ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} id="RouterNavLink" to='/user'>My Info</NavLink></NavDropdown.Item>
+                    <Signup className='signup' />
 
-                            <NavDropdown.Item ><img src={props.reducer.user.url} height='50' width='50' /></NavDropdown.Item>
-                            <NavDropdown.Divider />
+                    <If condition={!props.reducer.loggedIn} ><Then>
+                        <Login />
 
-                            <div className='loginIcon'>
-                                <Login />
-                            </div>
+                    </Then></If>
 
-                        </NavDropdown>
-                        <img src={props.reducer.user.url} height='45' width='45' />
 
-                    </Then>
-                </If>
-            </Form>
+                </Nav>
+                <Nav>
+                    <If condition={props.reducer.admin} ><Then>
+
+                        <Nav.Link ><NavLink to='/admintool'><img src='https://cdn2.hubspot.net/hub/189007/file-526634248-png/google-apps-admin-panel-icon.png' height='35' width='35' /></NavLink></Nav.Link>
+                    </Then></If>
+
+                    <If condition={props.reducer.loggedIn}>
+                        <Then>
+                            <NavDropdown style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif", fontWeight: 'bold' }} title={props.reducer.user.username} id="collasible-nav-dropdown">
+                                <NavDropdown.Item ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} id="RouterNavLink" to='/user'>My Info</NavLink></NavDropdown.Item>
+
+                                <NavDropdown.Item ><img src={props.reducer.user.url} height='50' width='50' /></NavDropdown.Item>
+                                <NavDropdown.Divider />
+
+                                <div className='loginIcon'>
+                                    <Login />
+                                </div>
+
+                            </NavDropdown>
+                            <img className='profileImg' src={props.reducer.user.url} height='45' width='45' />
+
+                        </Then>
+                    </If>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
-        //  {/* </div> */}
     )
-
 }
+// const Header = (props) => {
+
+//     return (
+//         // <div className='mainHeader'>
+//         <Navbar bg="light" variant="dark" sticky="top">
+//             <Navbar.Brand><NavLink to='/'><img src='https://i.ibb.co/k3Rq69C/Screenshot-477.png' style={{ borderRadius: '12px' }} height='45' width='50' /></NavLink></Navbar.Brand>
+//             <If condition={props.reducer.loggedIn}><Then>
+
+//                 <Nav className="mr-auto">
+//                     <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/data'>Courses</NavLink></Nav.Link>
+//                     <Nav.Link ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} to='/code'>Code Editor</NavLink></Nav.Link>
+//                     {/* <Nav.Link ><NavLink to='/feedback'><img src='https://cdn1.iconfinder.com/data/icons/feedback-review/64/x-05-512.png' height='30' width='35' /></NavLink></Nav.Link> */}
+//                     <ChatBox />
+
+//                 </Nav>
+//             </Then></If>
+//             {/* <Form inline> */}
+
+//             <Signup className='signup' />
+
+//             <If condition={!props.reducer.loggedIn} ><Then>
+//                 <Login />
+
+//             </Then></If>
+
+
+
+//             <If condition={props.reducer.admin} ><Then>
+
+//                 <Nav.Link ><NavLink to='/admintool'><img src='https://cdn2.hubspot.net/hub/189007/file-526634248-png/google-apps-admin-panel-icon.png' height='35' width='35' /></NavLink></Nav.Link>
+//             </Then></If>
+
+//             <If condition={props.reducer.loggedIn}>
+//                 <Then>
+//                     <NavDropdown style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} title={props.reducer.user.username} id="basic-nav-dropdown">
+//                         <NavDropdown.Item ><NavLink style={{ textDecoration: 'none', fontFamily: " 'Roboto', sansSerif" }} id="RouterNavLink" to='/user'>My Info</NavLink></NavDropdown.Item>
+
+//                         <NavDropdown.Item ><img src={props.reducer.user.url} height='50' width='50' /></NavDropdown.Item>
+//                         <NavDropdown.Divider />
+
+//                         <div className='loginIcon'>
+//                             <Login />
+//                         </div>
+
+//                     </NavDropdown>
+//                     <img src={props.reducer.user.url} height='45' width='45' />
+
+//                 </Then>
+//             </If>
+//             {/* </Form> */}
+//         </Navbar>
+//         //  {/* </div> */}
+//     )
+
+// }
 
 const mapStateToProps = state => ({
     reducer: state.reducer
@@ -93,4 +146,4 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps
-)(Header);
+)(nav);
